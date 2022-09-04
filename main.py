3296f29bd0ffe09b287ee56892b553fcf60e9d2f
WongYC19 <ycfkjc@Hotmail.com>
2022-09-05 00:31:41 +0800

    Update pickle

--- main.py
+++ main.py
@@ -24,21 +24,19 @@ pickler = Pickler()
 
 sp = SharePrice()
 stock_codes = sp.get_stock_codes()
-share_prices = pickler.read("prices.pkl")
-# share_prices = sp.get_share_price(stock_codes.index)
+# share_prices = pickler.read("prices.pkl")
+share_prices = sp.get_share_price(stock_codes.index)
 pickler.write(share_prices, "prices.pkl", merge=False)
 signals = run_all_shares(share_prices, MA_3)
 signals.to_pickle("MA_3_Signals.pkl")
 
+# %%
+from theia.bursamktplc import Bursa
 bursa = Bursa()
 # tickers = bursa.get_stock_tickers()
 # metadata = bursa.get_stock_metadata(tickers)
 # tickers.to_csv("tickers.csv", index=False)
 # metadata.to_csv("metadata.csv", index=False)
-
-# %%
-from theia.bursamktplc import Bursa
-bursa = Bursa()
 tickers = bursa.get_stock_tickers()
 metadata = bursa.get_stock_metadata(tickers)
 metadata[bursa.metadata_fields].to_csv("metadata.csv", index=False)
